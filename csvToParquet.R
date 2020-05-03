99a88e6b59d4e825fe47d6fdd35a00f605d4680b
Jaak Randmets <jaak.ra+github@gmail.com>
2020-05-04 01:19:25 +0300

    Add a script for converting CSV files to parquet.

---
+++ csvToParquet.R
@@ -0,0 +1,143 @@
+library(data.table)
+library(arrow, warn.conflicts = FALSE)
+
+ReadDungeonInfo <- function() {
+    colClasses <- c("character","character")
+    result <- fread("data/static/dungeon-info.csv",
+                    head=TRUE,
+                    sep=";",
+                    colClasses=colClasses,
+                    key=c("Dungeon"))
+    return(result)
+}
+
+ReadPeriodIndex <- function() {
+    colClasses <- c("character","integer","integer64", "integer64")
+    result <- fread("data/static/period-index.csv",
+                    head=TRUE,
+                    sep=";",
+                    colClasses=colClasses,
+                    key=c("Region", "StartTimestamp", "EndTimestamp"))
+    return(result)
+}
+
+ReadDungeonTimeLimits <- function() {
+    colClasses <- c("character","integer","integer", "integer")
+    result <- fread("data/static/dungeon-time-limits.csv",
+                    head=TRUE,
+                    sep=";",
+                    colClasses=colClasses,
+                    key=c("Dungeon", "PeriodStart", "PeriodEnd"))
+    return(result)
+}
+
+ReadCSVFile <- function(fname) {
+    colClasses <-
+        c("character","character","character",
+          "integer64","integer","integer",
+          "character","character")
+    colClasses <- c(colClasses, rep("integer", times = 4*6))
+    result <- fread(fname,
+                    head=TRUE,
+                    sep=";",
+                    colClasses=colClasses)
+    return(unique(result))
+}
+
+DungeonInfo <- ReadDungeonInfo()
+PeriodIndex <- ReadPeriodIndex()
+DungeonTimeLimits <- ReadDungeonTimeLimits()
+
+Preprocess <- function(Tbl) {
+    Tbl[, Timestamp2 := Timestamp]
+
+    Tbl <- foverlaps(Tbl, PeriodIndex,
+        by.x = c("Region", "Timestamp", "Timestamp2"),
+        by.y = c("Region", "StartTimestamp", "EndTimestamp"),
+        type = "within",
+        mult = "first")
+
+    # Clear useless fields added by foverlaps
+    Tbl[, StartTimestamp := NULL]
+    Tbl[, EndTimestamp := NULL]
+    Tbl[, Timestamp2 := NULL]
+
+    Tbl[,TimeMinutes:=Duration/60000]
+
+    Tbl[, PeriodId2 := PeriodId]
+    Tbl <- foverlaps(Tbl, DungeonTimeLimits,
+        by.x = c("Dungeon", "PeriodId", "PeriodId2"),
+        by.y = c("Dungeon", "PeriodStart", "PeriodEnd"),
+        type = "within",
+        mult = "first")
+    if (anyNA(Tbl$TimeLimit)) {
+        cat("WARNING! For some dungeons time limit is missing! Update dungeon-time-limits.csv file.")
+    }
+
+    Tbl[, PeriodId2 := NULL]
+    Tbl[, PeriodStart := NULL]
+    Tbl[, PeriodEnd := NULL]
+    Tbl[, Success:=TimeMinutes<=TimeLimit]
+    Tbl[, TimeLimit:=NULL]
+
+    Tbl[, Timestamp:=as.POSIXct(Timestamp/1000, origin="1970-01-01", tz="UTC")]
+
+    # Remove few more redundant columns
+    Tbl[,Duration:=NULL]
+
+    # This is weird:
+    setcolorder(Tbl, c("Region", "PeriodId", "Faction", "Dungeon", "KeystoneLevel", "Timestamp", "TimeMinutes", "Success"))
+    return(Tbl)
+}
+
+SaveAsParquet <- function(df, name) {
+    dbSchema <- schema(
+        Region = string(),
+        PeriodId = int16(),
+        Faction = string(),
+        Dungeon = string(),
+        KeystoneLevel = int8(),
+        Timestamp = timestamp("ms", timezone = "UTC"),
+        TimeMinutes = float32(),
+        Success = bool(),
+        Tank = string(),
+        Healer = string(),
+        NumFrostDk = int8(),
+        NumUnholyDk = int8(),
+        NumHavocDh = int8(),
+        NumBalanceDruid = int8(),
+        NumFeralDruid = int8(),
+        NumBeastMasterHunter = int8(),
+        NumMarksmanshipHunter = int8(),
+        NumSurvivalHunter = int8(),
+        NumArcaneMage = int8(),
+        NumFireMage = int8(),
+        NumFrostMage = int8(),
+        NumWindwalkerMonk = int8(),
+        NumRetributionPaladin = int8(),
+        NumShadowPriest = int8(),
+        NumAssassinationRogue = int8(),
+        NumOutlawRogue = int8(),
+        NumSubtletyRogue = int8(),
+        NumElementalShaman = int8(),
+        NumEnhancementShaman = int8(),
+        NumAfflictionWarlock = int8(),
+        NumDemonologyWarlock = int8(),
+        NumDestructionWarlock = int8(),
+        NumArmsWarrior = int8(),
+        NumFuryWarrior = int8()
+    )
+
+    Tbl <- Table$create(df, schema = dbSchema)
+    write_parquet(Tbl, name)
+}
+
+args <- commandArgs(TRUE)
+
+for (file in args) {
+    cat(paste0("Loading file ", file, "\n"))
+    Df <- ReadCSVFile(file)
+    Df <- Preprocess(Df)
+    outFileName <- gsub(".csv$", ".parquet", file)
+    SaveAsParquet(Df, outFileName)
+}
